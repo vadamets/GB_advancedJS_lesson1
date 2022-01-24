2c8f95015bfe1b0b2e874e9c4539ab8ef3c44a82
Vladimir <vadamets@gmail.com>
2022-01-25 00:40:03 +0300

    done homework

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,36 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const miniCss = require('mini-css-extract-plugin')
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-    },
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
 
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: '[name].bundle.js',
-    },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.(s*)css$/,
+      use: [
+        miniCss.loader,
+        'css-loader',
+        'sass-loader',
+      ]
+    }]
+  },
 
-    plugins: [
-      new HtmlWebpackPlugin({
-          title: 'GeekBrains Shop JS2',
-          template: path.resolve(__dirname, './public/template.html'), // шаблон
-          filename: 'index.html', // название выходного файла
-      }),
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'GeekBrains Shop JS2',
+      template: path.resolve(__dirname, './public/template.html'), // шаблон
+      filename: 'index.html', // название выходного файла
+    }),
+    new miniCss({
+      filename: 'style.css',
+    }),
+  ],
 }
